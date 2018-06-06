154ccb52153c9c949fb088a0fdee2894dc6173c1
Kévin Leroy <kevin.leroy@thalesgroup.com>
2018-06-06 15:07:20 +0200

    @Controller modifié en @ControllerREST

--- src/main/java/com/thales/Spring/HelloController.java
+++ src/main/java/com/thales/Spring/HelloController.java
@@ -1,11 +1,11 @@
 package com.thales.Spring;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/HelloWorld")
 public class HelloController {
 
